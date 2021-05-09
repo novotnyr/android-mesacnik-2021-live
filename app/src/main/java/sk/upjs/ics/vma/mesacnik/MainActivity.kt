d74ad512b7d1dc25c5f99f43b21ebf0bd45a6dbb
Robert Novotny <novotnr0@gmail.com>
2021-05-10 01:32:26 +0200

    Uvodny import

---
+++ app/src/main/java/sk/upjs/ics/vma/mesacnik/MainActivity.kt
@@ -0,0 +1,11 @@
+package sk.upjs.ics.vma.mesacnik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
